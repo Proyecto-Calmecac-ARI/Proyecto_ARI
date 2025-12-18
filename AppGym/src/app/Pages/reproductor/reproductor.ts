6941852aa2a832ca254e06ac14b02c4e75846c1f
GJair-GV <gerardojairgarciavilla47@gmail.com>
2025-12-17 21:06:59 -0600

    reproductor

--- AppGym/src/app/Pages/reproductor/reproductor.ts
+++ AppGym/src/app/Pages/reproductor/reproductor.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { ListaVideo } from "../../Components/lista-video/lista-video";
+import { ReproductorVideo } from "../../Components/reproductor-video/reproductor-video";
 
 @Component({
   selector: 'app-reproductor',
-  imports: [],
+  imports: [ListaVideo, ReproductorVideo],
   templateUrl: './reproductor.html',
   styleUrl: './reproductor.scss',
 })
