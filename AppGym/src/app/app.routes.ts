931cb250b974f46d6c6971da94f69b554db657a0
GJair-GV <gerardojairgarciavilla47@gmail.com>
2025-12-17 21:38:25 -0600

    Cambio de mayusculas a minusculas en las rutas

--- AppGym/src/app/app.routes.ts
+++ AppGym/src/app/app.routes.ts
@@ -2,14 +2,12 @@ import { Routes } from '@angular/router';
 import { Login } from './Pages/login/login';
 import { Reproductor } from './Pages/reproductor/reproductor';
 import { Dasboard } from './Pages/dasboard/dasboard';
-import { DasboardWickOutPlan } from './Pages/dasboard-wick-out-plan/dasboard-wick-out-plan';
 import { Formulario } from './Pages/formulario/formulario';
 export const routes: Routes = [
-    { path: 'Login', component: Login },
-    { path: 'Formulario', component: Formulario },
-    { path: 'DashboardWithOutPlan', component: DasboardWickOutPlan },
-    { path: 'Dashboard', component: Dasboard },
-    { path: 'Reproductor', component: Reproductor },
-    { path: '', redirectTo: 'Login', pathMatch: 'full'},
-    { path: '**', redirectTo: 'Login'}
+    { path: 'login', component: Login },
+    { path: 'formulario', component: Formulario },
+    { path: 'dashboard', component: Dasboard },
+    { path: 'reproductor', component: Reproductor },
+    { path: '', redirectTo: 'login', pathMatch: 'full'},
+    { path: '**', redirectTo: 'login'}
 ];
