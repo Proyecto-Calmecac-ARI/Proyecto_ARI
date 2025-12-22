d74c8031e9b55c54a8de9bcf562ebb2bd9621fd2
AngelesGG1 <mangelesgarido@gmail.com>
2025-12-21 22:20:12 -0600

    Implementacion del login

--- AppGym/src/app/Components/login-up/login-up.ts
+++ AppGym/src/app/Components/login-up/login-up.ts
@@ -2,14 +2,109 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CustomFonts } from '../../enums/fonts.enum';
 import { getFont } from '../../utils/font.util';
-
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { UserService } from '../../Services/UserService';
+import { UserInterface } from '../../../interfaces/UserInterface';
 @Component({
   selector: 'app-login-up',
-  imports: [ CommonModule ],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login-up.html',
   styleUrl: './login-up.scss',
 })
 export class LoginUp {
-  customFonts = CustomFonts
-  getFont = getFont
+  customFonts = CustomFonts;
+  getFont = getFont;
+  correo: string = '';
+  contrasena: string = '';
+  constructor(private userService: UserService, private router: Router) {}
+  login() {
+    // Validación campos vacíos
+    if (!this.correo || !this.contrasena) {
+      alert('Debes llenar todos los campos');
+      return;
+    }
+    // Correo inválido
+    if (!this.isValidCorreo(this.correo)) {
+      alert('Ingresa un correo electrónico válido');
+      return;
+    }
+    const usuario = this.userService.buscarUsuario(this.correo, this.contrasena);
+    //  Usuario no existe
+    if (!usuario) {
+      alert('El usuario no existe, debes registrarte');
+      return;
+    }
+    // Validar vencimiento del plan
+    if (usuario.planActivo && usuario.fechaExpiracionPlan) {
+      const today = new Date();
+      if (today > usuario.fechaExpiracionPlan) {
+        alert(
+          'Error, detectamos que no has renovado tu membresía, por favor renuévala y vuelve a entrar.'
+        );
+        return;
+      }
+    }
+    // Guardar usuario global
+    this.userService.guardarUsuarioActual(usuario);
+    // Redirecciones
+    this.router.navigate(['/dashboard']);
+  }
+  register() {
+    // Campos vacíos
+    if (!this.correo || !this.contrasena) {
+      alert('Debes llenar todos los campos');
+      return;
+    }
+    // Correo inválido
+    if (!this.isValidCorreo(this.correo)) {
+      alert('Ingresa un correo electrónico válido');
+      return;
+    }
+    // Buscar usuario existente
+    const usuario = this.userService.buscarUsuario(this.correo, this.contrasena);
+    //  Si ya existe
+    if (usuario) {
+      alert('El usuario existe, debes iniciar sesión');
+      return;
+    }
+    // Crear nuevo usuario (registro inicial)
+    const nuevoUsuario: UserInterface = {
+      correo: this.correo,
+      contrasena: this.contrasena,
+      nombreUsuario: undefined,
+      apellidosUsuario: undefined,
+      edad: undefined,
+      estaturaCm: undefined,
+      tipoCuerpo: undefined,
+      objetivo: undefined,
+      frecuenciaAsistencia: undefined,
+      tipoAlimentacion: undefined,
+      frecuenciaCardiaca: undefined,
+      oxigenacion: undefined,
+      trofeos: 0,
+      caloriasQuemadas: undefined,
+      planActivo: false,
+      rutinaActiva: undefined,
+      asistencias: undefined,
+      planAsociado: undefined,
+      metodoPago: undefined,
+      fechaCompraPlan: undefined,
+      fechaExpiracionPlan: undefined,
+    };
+    // Guardar en arreglo global
+    this.userService.agregarUsuario(nuevoUsuario);
+    // Guardar como usuario actual
+    this.userService.guardarUsuarioActual(nuevoUsuario);
+    // Ir al formulario para completar perfil
+    this.router.navigate(['/formulario']);
+  }
+  scrollAPlanes() {
+    const element = document.getElementById('planes');
+    element?.scrollIntoView({ behavior: 'smooth' });
+  }
+  isValidCorreo(correo: string): boolean {
+    const correoRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return correoRegex.test(correo);
+  }
 }
---
+++ AppGym/src/app/Services/UserService.ts
@@ -0,0 +1,136 @@
+import { Injectable } from '@angular/core';
+import { UserInterface } from '../../interfaces/UserInterface';
+@Injectable({
+    providedIn: 'root'
+})
+export class UserService {
+    // Arreglo global de usuarios
+    private usuarios: UserInterface[] = [
+        {
+            correo: 'prueba1@gmail.com',
+            contrasena: '1234',
+            nombreUsuario: 'Prueba',
+            apellidosUsuario: 'Prueba1',
+            edad: 18,
+            estaturaCm: 157,
+            tipoCuerpo: 'Delgado',
+            objetivo: 'Perder Peso',
+            frecuenciaAsistencia: 'Ocacional (1 vez/semana)',
+            tipoAlimentacion: 'Flexible / Sin plan especifico',
+            frecuenciaCardiaca: undefined,
+            oxigenacion: undefined,
+            trofeos: 0,
+            caloriasQuemadas: undefined,
+            planActivo: false,
+            rutinaActiva: undefined,
+            asistencias: undefined,
+            planAsociado: undefined,
+            metodoPago: undefined,
+            fechaCompraPlan: undefined,
+            fechaExpiracionPlan: undefined
+        },
+        {
+            correo: 'prueba2@gmail.com',
+            contrasena: '1234',
+            nombreUsuario: 'Prueba',
+            apellidosUsuario: 'Prueba2',
+            edad: 18,
+            estaturaCm: 157,
+            tipoCuerpo: 'Delgado',
+            objetivo: 'Perder Peso',
+            frecuenciaAsistencia: 'Ocacional (1 vez/semana)',
+            tipoAlimentacion: 'Flexible / Sin plan especifico',
+            frecuenciaCardiaca: 123,
+            oxigenacion: 86,
+            trofeos: 100,
+            caloriasQuemadas: 2000,
+            planActivo: true,
+            rutinaActiva: undefined,
+            asistencias: undefined,
+            planAsociado: {
+                nombrePlan: 'Plan PowerPlus',
+                descripcionPlan: 'Pensado para personas que ya entrenan de manera regular. Incluye rutinas personalizadas por objetivo, monitoreo avanzado de oxigenación, historial de progreso y ranking por categoría (fuerza, cardio, resistencia).',
+                precioPlan: 349
+            },
+            metodoPago: {
+                nombreTarjeta: 'ejemplo numero 1',
+                cvv: '123',
+                fechaExpiracion: '01/26',
+                numeroTarjeta: '8282 8383 8383 8383'
+            },
+            fechaCompraPlan: new Date('2024-01-01'),
+            fechaExpiracionPlan: new Date('2025-01-01')
+        },
+        {
+            correo: 'prueba3@gmail.com',
+            contrasena: '1234',
+            nombreUsuario: 'Prueba',
+            apellidosUsuario: 'Prueba3',
+            edad: 18,
+            estaturaCm: 157,
+            tipoCuerpo: 'Delgado',
+            objetivo: 'Perder Peso',
+            frecuenciaAsistencia: 'Ocacional (1 vez/semana)',
+            tipoAlimentacion: 'Flexible / Sin plan especifico',
+            frecuenciaCardiaca: 123,
+            oxigenacion: 86,
+            trofeos: 100,
+            caloriasQuemadas: 2000,
+            planActivo: true,
+            rutinaActiva: {
+                nombreLista: 'Quema Grasa',
+                listaVideos: [
+                    {
+                        nombreVideo: 'Video 1',
+                        urlVideo: 'urlVideo',
+                        videoVisto: false,
+                        nombreCoach: 'Angeles'
+                    }
+                ],
+                imagenRutina: 'urlImagen'
+            },
+            asistencias: [
+                {
+                    fechaAsistencia: undefined,
+                    dia: '01',
+                    mes: '01',
+                    anio: '2025',
+                    tiempoCronometroSegundos: 1000
+                }
+            ],
+            planAsociado: {
+                nombrePlan: 'Plan EliteAthlete',
+                descripcionPlan: 'La experiencia completa. Acceso a rutinas avanzadas exclusivas, oxigenación en tiempo real, rankings dinámicos, recomendaciones inteligentes según desempeño y sistema de logros gamificado.',
+                precioPlan: 499
+            },
+            metodoPago: {
+                nombreTarjeta: 'ejemplo numero 1',
+                cvv: '123',
+                fechaExpiracion: '01/26',
+                numeroTarjeta: '8282 8383 8383 8383'
+            },
+            fechaCompraPlan: new Date('2025-01-01'),
+            fechaExpiracionPlan: new Date('2027-01-01')
+        }
+    ];
+    // Usuario logueado este es el usuario actual
+    usuarioActual: UserInterface | null = null;
+    // obtiene todos los usuarios
+    obtenerUsuarios() {
+        return this.usuarios;
+    }
+    // Busca al usuario con el que se requiere iniciar sesion
+    buscarUsuario(correo: string, contrasena: string): UserInterface | undefined {
+        return this.usuarios.find(
+            usuario => usuario.correo === correo && usuario.contrasena === contrasena
+        );
+    }
+    // Esta funcion agrega un usuario nuevo al arreglo usuarios 
+    agregarUsuario(usuario: UserInterface) {
+        this.usuarios.push(usuario);
+    }
+    // Guarda los datos para el usuario actual
+    guardarUsuarioActual(usuario: UserInterface) {
+        this.usuarioActual = usuario;
+    }
+}
